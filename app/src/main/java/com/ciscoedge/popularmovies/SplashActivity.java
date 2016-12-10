208006814c9324f3e3899af4afbcf07c44ae9e37
Vladimir Midy <vladimir@ciscoedge.com>
2016-12-09 20:08:14 -0500

    Final.

---
+++ app/src/main/java/com/ciscoedge/popularmovies/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.ciscoedge.popularmovies;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MovieMainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
